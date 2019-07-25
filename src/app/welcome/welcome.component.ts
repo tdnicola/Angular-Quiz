a488abf8d2f75aa8dd753c7330d82b4579156131
tdnicola <tdnicola@gmail.com>
2019-07-25 16:10:30 -0400

    working on welcome page to display bjj/mma quiz.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { QuestionsService } from '../questions.service';
+import { Quiz } from '../quiz.model';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+export class WelcomeComponent {
+  quiz: Quiz[];
 
-  ngOnInit() {
-  }
+  constructor(private questionsService: QuestionsService) {
 
+    this.questionsService.getQuizzes()
+       .subscribe(quiz => {
+       this.quiz = quiz;
+      });
+   }
 }
